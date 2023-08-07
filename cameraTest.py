b74c36d5363917f819828c214a8a09cee1c69cd2
Saikrishna.V <94283611+saikrishna2003@users.noreply.github.com>
2023-08-07 21:52:52 +0530

    Add files via upload

---
+++ cameraTest.py
@@ -0,0 +1,14 @@
+import cv2
+import time
+
+cam=cv2.VideoCapture(0)
+time.sleep(1)
+while True:
+    _,img=cam.read()
+    cv2.imshow("camerafeed", img)
+    key =cv2.waitKey(1) & 0xFF
+    if key==ord("q"):
+        break
+    
+cam.release()
+cv2.destroyAllWindows()
